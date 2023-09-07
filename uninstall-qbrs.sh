9456701859c62e952302da662861284a575a6f9d
CCCOrz <godogpt@gmail.com>
2023-09-07 18:02:29 +0800

    修改：一键卸载时保留配置文件

--- uninstall-qbrs.sh
+++ uninstall-qbrs.sh
@@ -5,7 +5,6 @@ if [[ -f "/etc/systemd/system/qbrs.service" ]]; then
     systemctl disable qbrs
     
     rm /usr/local/bin/qbrs_*
-    rm /usr/local/bin/config.env
 
     rm /etc/systemd/system/qbrs.service
     systemctl daemon-reload
