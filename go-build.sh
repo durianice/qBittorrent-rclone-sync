bb81a6da87b43db230e66b4f93f80fd8d3efa47c
CCCOrz <godogpt@gmail.com>
2023-09-07 17:34:25 +0800

    修改：编译时环境变量

--- go-build.sh
+++ go-build.sh
@@ -35,7 +35,7 @@ fi
 archs=(amd64 arm64 arm ppc64le ppc64 s390x) 
 for arch in ${archs[@]} 
 do
-    env GOOS=linux GOARCH=${arch} go build -o ./build/qbrs_${arch} -v main.go
+    env CGO_ENABLED=0 GOOS=linux GOARCH=${arch} go build -o ./build/qbrs_${arch} -v main.go
 done 
 
 echo "已编译以下平台："
